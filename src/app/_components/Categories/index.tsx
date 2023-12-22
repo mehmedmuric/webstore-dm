b8904cdf265da8d926a51a3fb336b9179d9de863
Mehmed Muric <mehmedmuric22@gmail.com>
2023-12-22 17:04:05 +0100

    category map

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './index.module.scss'
 import Link from 'next/link'
 import { Category } from '../../../payload/payload-types'
+import CategoryCard from './CategoryCard'
 
 const Categories = ({categories}: {categories: Category[]}) => {
   return (
@@ -13,7 +14,7 @@ const Categories = ({categories}: {categories: Category[]}) => {
 
       <div className={classes.list}>
         {categories.map((category) => (
-            <CategoryCard />
+            <CategoryCard key={category.id} category={category}/>
         ))}
       </div>
     </section>
